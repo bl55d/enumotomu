adf8d18326043115d12e5b6b5bf69ae3084e8f91
Aldi Satria <52058660+acvn@users.noreply.github.com>
2024-02-01 09:24:06 +0700

    asd

--- run.sh
+++ run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+# Show banner
 cat ~/enumotomu/banner.txt
 
+# Make output directory
 mkdir enumotomu-$1
 
 # sslscan
 echo -e "sslscan start ... \c"
 if ~/./sslscan/sslscan "$1" >> "enumotomu-$1/sslscan-$1.txt"; then
-    echo " done!"
-else
-    echo " error: $?"
-fi
-
-# nikto
-echo -e "\nnikto start ... \c" 
-sed 's///g' 
-if ~/./nikto/program/nikto.pl -h "$1" >> "enumotomu-$1/nikto-$1.txt"; then
     echo " done!"
 else
     echo " error: $?"
@@ -32,6 +25,15 @@ fi
 # nuclei
 echo -e "\nnuclei start ... \c"
 if nuclei -u "$1" --silent >> "enumotomu-$1/nuclei-$1.txt"; then
+    echo " done!"
+else
+    echo " error: $?"
+fi
+
+# nikto
+echo -e "\nnikto start ... \c" 
+sed 's///g' 
+if ~/./nikto/program/nikto.pl -h "$1" >> "enumotomu-$1/nikto-$1.txt"; then
     echo " done!"
 else
     echo " error: $?"
